1445bedfc45b47659d86a486c8a8d1ab107527f6
xiangyu <wuxiangyu@berkeley.edu>
2021-02-12 17:59:54 -0800

    Increase the SYS_STATUS frequency from 5Hz to 100Hz, to get voltage measurement at 100Hz.

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1669,7 +1669,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("POSITION_TARGET_LOCAL_NED", 10.0f);
 		configure_stream_local("RC_CHANNELS", 20.0f);
 		configure_stream_local("SERVO_OUTPUT_RAW_0", 10.0f);
-		configure_stream_local("SYS_STATUS", 5.0f);
+		configure_stream_local("SYS_STATUS", 100.0f);
 		configure_stream_local("SYSTEM_TIME", 1.0f);
 		configure_stream_local("TIMESYNC", 10.0f);
 		configure_stream_local("TRAJECTORY_REPRESENTATION_WAYPOINTS", 5.0f);
